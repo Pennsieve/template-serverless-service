6712682e7fd0e9b2595b74346312254bd0eeb661
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-05-08 11:28:27 -0400

    adding datadog logging to service

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,14 @@ variable "image_tag" {}
 variable "lambda_bucket" {
   default = "pennsieve-cc-lambda-functions-use1"
 }
+
+locals {
+  domain_name = data.terraform_remote_state.account.outputs.domain_name
+  hosted_zone = data.terraform_remote_state.account.outputs.public_hosted_zone_id
+
+  common_tags = {
+    aws_account      = var.aws_account
+    aws_region       = data.aws_region.current_region.name
+    environment_name = var.environment_name
+  }
+}
