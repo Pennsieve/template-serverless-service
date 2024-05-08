af6f9dd92a7705d85e52fe31733812eb45de0809
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-05-08 11:46:53 -0400

    adding ssm access

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -59,4 +59,17 @@ data "aws_iam_policy_document" "service_iam_policy_document" {
     resources = ["*"]
   }
 
+  statement {
+    sid    = "SSMPermissions"
+    effect = "Allow"
+
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+    ]
+
+    resources = ["arn:aws:ssm:${data.aws_region.current_region.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.environment_name}/${var.service_name}/*"]
+  }
+
 }
